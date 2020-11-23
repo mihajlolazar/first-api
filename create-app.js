5c7a3d77b503421ce71677cdb2609bef20ef0c70
Mihajlo Lazar <mihajlolazar@hotmail.com>
2020-11-23 23:26:43 +0100

    update

--- create-app.js
+++ create-app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dbUser = process.env.DB_USER || '';
-const dbPassword = process.env.DB_USER || '';
+const dbPassword = process.env.DB_PASSWORD || '';
 const dbName = process.env.DB_NAME || '';
 
-console.log('start');
-console.log(process.env);
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASSWORD);
+console.log(process.env.DB_NAME);
 
 // Set up Mongoose and connect to DB
 // mongoose.set('useFindAndModify', false);
