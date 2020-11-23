2a9e99a74faa4fddc9b84128445f9ec25e406953
Mihajlo Lazar <mihajlolazar@hotmail.com>
2020-11-23 23:44:23 +0100

    update

--- create-app.js
+++ create-app.js
@@ -10,16 +10,16 @@ console.log(process.env.DB_PASSWORD);
 console.log(process.env.DB_NAME);
 
 // Set up Mongoose and connect to DB
-mongoose.set('useFindAndModify', false);
-const db = mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.010pa.mongodb.net/${dbName}?retryWrites=true&w=majority`).catch(e => {
-    console.log(e);
-});
-mongoose.Promise = global.Promise;
+// mongoose.set('useFindAndModify', false);
+// const db = mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.010pa.mongodb.net/${dbName}?retryWrites=true&w=majority`).catch(e => {
+//     console.log(e);
+// });
+// mongoose.Promise = global.Promise;
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello world');
+    res.send('Hello world ' + process.env.DB_USER);
 });
 
 module.exports = app;
