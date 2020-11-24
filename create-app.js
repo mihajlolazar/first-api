47f7987e34f438d42aef8f3131a02d10ae4ec080
Mihajlo Lazar <mihajlolazar@hotmail.com>
2020-11-24 06:36:57 +0100

    jest node.js bugfix

--- create-app.js
+++ create-app.js
@@ -4,22 +4,22 @@ const dbUser = process.env.DB_USER || '';
 const dbPassword = process.env.DB_PASSWORD || '';
 const dbName = process.env.DB_NAME || '';
 
-console.log(process.env.NODE_ENV);
-console.log(process.env.DB_USER);
-console.log(process.env.DB_PASSWORD);
-console.log(process.env.DB_NAME);
-
 // Set up Mongoose and connect to DB
-// mongoose.set('useFindAndModify', false);
-// const db = mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.010pa.mongodb.net/${dbName}?retryWrites=true&w=majority`).catch(e => {
-//     console.log(e);
-// });
-// mongoose.Promise = global.Promise;
+mongoose.set('useFindAndModify', false);
 
+if( dbUser && dbPassword && dbName ) {
+    const db = mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.010pa.mongodb.net/${dbName}?retryWrites=true&w=majority`).catch(e => {
+        console.log(e);
+    });
+}
+
+mongoose.Promise = global.Promise;
+
+// init express server
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello world ' + (process.env.DB_USER || ''));
+    res.send('Hello world');
 });
 
 module.exports = app;
