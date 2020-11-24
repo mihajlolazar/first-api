6829084e4735f486d9597d1b7a43b6b3470efb62
Mihajlo Lazar <mihajlolazar@hotmail.com>
2020-11-24 08:28:05 +0100

    test update

--- src/tests/index.spec.js
+++ src/tests/index.spec.js
@@ -5,7 +5,7 @@ const createApp = require('../create-app');
 
 const mockMongoose = new MockMongoose(mongoose);
 
-jest.setTimeout(30000);
+// jest.setTimeout(5000);
 
 
 let app;
@@ -14,11 +14,12 @@ beforeAll(async function (done) {
     await mockMongoose.prepareStorage();
     await mongoose.connect('mongodb://example.com/TestingDB');
     app = await createApp();
-
+    done()
 });
 
-afterAll(async function () { 
+afterAll(async function (done) { 
     await mongoose.connection.close()
+    done()
 })
 
 describe('GET /', function () {
@@ -32,35 +33,5 @@ describe('GET /', function () {
     });
 });
 
-
-describe('Products Endpoint', function () {
-    let product;
-
-    it('create a product',function (done) {
-      request(app)
-        .post('/products')
-        .set('Accept', 'application/json')
-        .set('Content-Type', 'application/json')
-        .expect('Content-Type', /json/)
-        .send({title: 'test', price: '100'})
-        .expect(200)
-        .then(function (res) {
-            product = res.body
-            console.log({product})
-            expect(product).toHaveProperty("title", "test")
-            expect(product).toHaveProperty("price", 100)
-            done();
-        });
-    });
-
-    it('return the product', function (done) {
-        request(app)
-            .get('/products/' + product._id)
-            .expect(200)
-            .then(function (res) {
-                expect(res.body).toEqual(product);
-                done();
-            });
-    });
-});
+ 
 
